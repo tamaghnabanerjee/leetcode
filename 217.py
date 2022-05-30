10fdfc0f0b49318642d11be9d75692f47efc2298
Tamaghna Banerjee <tamaghna.banerjee@gmail.com>
2022-05-30 10:19:06 +0530

    added 217

--- 217.py
+++ 217.py
@@ -1,1 +1,7 @@
-print("demo")
+def contains_duplicate(nums):
+    hash_table=set()
+    for num in nums:
+        if num in hash_table:
+            return True
+        hash_table.add(num)
+    return False
